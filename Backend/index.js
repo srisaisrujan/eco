b3a5dd46758abeccf0d96970c2aae083da72612e
srisaisrujan <dowleswarapusrisaisrujan@gmail.com>
2024-08-01 17:30:08 +0530

    first commit
    310724

---
+++ Backend/index.js
@@ -0,0 +1,258 @@
+import express from "express";
+import pg from "pg";
+import jwt from "jsonwebtoken";
+import multer from "multer";
+import path from "path";
+import cors from "cors";
+import mongoose from "mongoose";
+import { log } from "console";
+
+const app=express();
+const port=4000;
+
+app.use(express.json());
+app.use(cors());
+
+//Database connection with mongodb
+mongoose.connect("mongodb+srv://prathap24reddy:1234@cluster0.mttstqv.mongodb.net/e-commerce");
+
+//API Creation
+app.get("/",(req,res)=>{
+    res.send("Express App is Running");
+});
+
+//Image Storage Engine
+const storage=multer.diskStorage({
+    destination:'./upload/images',
+    filename:(req,file,cb)=>{
+        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+});
+const upload=multer({storage:storage});
+
+//Creating Upload Endpoint for images
+app.use('/images',express.static('upload/images'));
+
+app.post("/upload",upload.single('product'),(req,res)=>{
+    res.json({
+        success:1,
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
+    });
+});
+
+//schema for creating products
+const Product=mongoose.model("Product",{
+    id:{
+        type:Number,
+        required:true,
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    new_price:{
+        type:Number,
+        required:true,
+    },
+    old_price:{
+        type:Number,
+        required:true,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    },
+    available:{
+        type:Boolean,
+        default:true,
+    },
+});
+
+app.post('/addproduct',async(req,res)=>{
+    let products=await Product.find({});
+    let id;
+    if(products.length>0){
+        let last_product_array=products.slice(-1);
+        let last_product=last_product_array[0];
+        id=last_product.id+1;
+    }else{id=1;}
+    const product=new Product({
+        id:id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+    });
+    console.log(product);
+    await product.save();
+    console.log("Saved");
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
+});
+
+//creating API for deleting products
+
+app.post('/removeproduct',async(req,res)=>{
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log("Removed");
+    res.json({
+        success:true,
+        name:req.body.name
+    });
+});
+
+//creating api for getting all products
+app.get('/allproducts',async(req,res)=>{
+    let products=await Product.find({});
+    console.log("All Products Fetched");
+    res.send(products);
+});
+
+//schema creating for User model
+const Users=mongoose.model('Users',{
+    name:{
+        type:String,
+    },
+    email:{
+        type:String,
+        unique:true,
+    },
+    password:{
+        type:String,
+    },
+    cartData:{
+        type:Object,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    }
+})
+
+//Creating endpoint for registering the user
+app.post('/signup',async(req,res)=>{
+    let check=await Users.findOne({email:req.body.email});
+    if(check){
+        return res.status(400).json({success:false,errors:"existing user found with the same email id."});
+    }
+    let cart={};
+    for (let i=0; i<300; i++) {
+        cart[i]=0;
+    }
+    const user=new Users({
+        name:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        cartData:cart,
+    })
+
+    await user.save();
+
+    const data={
+        user:{
+            id:user.id
+        }
+    }
+
+    const token=jwt.sign(data,'secret_ecom');
+    res.json({success:true,token})
+})
+
+//creating endpoint for login
+app.post('/login',async(req,res)=>{
+    let user=await Users.findOne({email:req.body.email});
+    if(user){
+        const passCompare=req.body.password===user.password;
+        if(passCompare){
+            const data={
+                user:{
+                    id:user.id
+                }
+            }
+            const token=jwt.sign(data,'secret_ecom');
+            res.json({success:true,token});
+        }else{
+            res.json({success:false,errors:"Wrong Password"});
+        }
+    }else{
+        res.json({success:false,errors:"Wrong Email Id."})
+    }
+})
+
+//creating endpoint for newcollection data
+app.get('/newcollection',async(req,res)=>{
+    let products=await Product.find({});
+    let newcollection=products.slice(1).slice(-8);
+    console.log("NewCollection Fetched.");
+    res.send(newcollection);
+})
+
+//creating endpoint for popular in women section
+app.get('/popularinwomen',async(req,res)=>{
+    let products=await Product.find({category:"women"});
+    let popular_in_women=products.slice(0,4);
+    console.log("Popular in women fetched.");
+    res.send(popular_in_women);
+})
+
+//creating middleware to fetch user
+    const fetchUser=async(req,res,next)=>{
+        const token=req.header('auth-token');
+        if(!token){
+            res.status(401).send({errors:"Please authenticate using valid token."})
+        }else{
+            try {
+                const data=jwt.verify(token,'secret_ecom');
+                req.user=data.user;
+                next();
+            } catch (error) {
+                res.status(401).send({errors:"Please authenticate using valid token."})
+            }
+        }
+    }
+
+//creating endpoint for adding products in cart data
+app.post('/addtocart',fetchUser ,async(req,res)=>{
+    console.log("added",req.body.itemId);
+    let userData=await Users.findOne({_id:req.user.id});
+    userData.cartData[req.body.itemId]+=1;
+    await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData});
+    res.send("Added")
+
+})
+
+//creating endpoint to remove product from cartdata
+app.post('/removefromcart',fetchUser,async(req,res)=>{
+    console.log("removed",req.body.itemId);
+    let userData=await Users.findOne({_id:req.user.id});
+    if(userData.cartData[req.body.itemId]>0)
+        userData.cartData[req.body.itemId]-=1;
+    await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData});
+    res.send("Removed")
+})
+
+//creating endpoint to get cart data
+app.post('/getcart',fetchUser,async(req,res)=>{
+    console.log("GetCart");
+    let userData=await Users.findOne({_id:req.user.id});
+    res.json(userData.cartData);
+})
+
+app.listen(port,(error)=>{
+    if(error){
+        console.log(error)
+    }else{
+        console.log(`Server is running on port: ${port}`);
+    }
+});
---
+++ README.md
@@ -0,0 +1,51 @@
+![Home](https://github.com/prathap24reddy/ecommerce/assets/136371579/5558b77f-d2d0-4adc-86d4-7d481955f1f4)
+![product list](https://github.com/prathap24reddy/ecommerce/assets/136371579/85a10bdf-4546-4724-a05f-409f97eddb87)
+![product](https://github.com/prathap24reddy/ecommerce/assets/136371579/36453352-0ea9-4094-a6f8-f195202de6e9)
+![signup](https://github.com/prathap24reddy/ecommerce/assets/136371579/0c25af03-a3ed-476b-88f0-c715efc11339)
+![add_product](https://github.com/prathap24reddy/ecommerce/assets/136371579/2fbda0ce-44b3-4eed-bd1f-4c2fba33cd14)
+![cart](https://github.com/prathap24reddy/ecommerce/assets/136371579/d0f83ac6-eb9e-48c0-9b3a-bc7b3f1d72d2)
+
+
+### E-commerce Full-Stack Website: Seamlessly Connecting Shoppers and Administrators
+
+Embark on a cutting-edge shopping experience with our responsive e-commerce full-stack website. From user-friendly features for shoppers to a powerful admin panel for efficient product management, this platform brings together technology and convenience. Let's explore the key components:
+
+### User-Centric Features
+
+**1. User Authentication:**
+   Sign up, log in, and log out securely to personalize your shopping journey. Our authentication system ensures the privacy and security of user accounts.
+
+**2. Dynamic Product Catalog:**
+   Explore a diverse range of products fetched through APIs, providing real-time updates on the latest trends and must-have items.
+
+**3. Effortless Cart Management:**
+   Add products to your cart seamlessly, review selections, and proceed through a smooth checkout process. The cart dynamically updates, offering transparency and convenience.
+
+**4. API Integration for Product Details:**
+   Product details are fetched through APIs, ensuring accurate and up-to-date information for an enhanced shopping experience.
+
+**5. Responsive Design:**
+   Enjoy a visually appealing and responsive design that adapts to various devices, providing a consistent and enjoyable shopping experience on desktops, tablets, and mobile devices.
+
+### Robust Admin Panel
+
+**6. Admin Dashboard:**
+   Administrators have access to a comprehensive dashboard that facilitates efficient monitoring of user activities, order management, and overall platform performance.
+
+**7. Product Management with Multer:**
+   The admin panel empowers administrators to view, update, and add new products effortlessly. Multer integration allows easy file uploads for vivid product images and detailed descriptions.
+
+**8. MongoDB Database:**
+   The backend is supported by MongoDB, a flexible and scalable NoSQL database, ensuring optimal data storage and retrieval.
+
+### Technologies Utilized
+
+**9. React for Frontend:**
+   Our frontend leverages React to provide a dynamic, interactive, and user-friendly interface, enhancing the overall shopping experience.
+
+**10. Node.js and Express.js for Server-Side Processing:**
+   Node.js and Express.js efficiently handle server-side processing, ensuring quick responses to user requests and seamless integration with the frontend.
+
+### Conclusion
+
+Our e-commerce full-stack website is a testament to modern web development practices, seamlessly integrating technology to offer a feature-rich, secure, and enjoyable online shopping experience. Whether you're a shopper exploring the latest products or an administrator managing the platform, our solution caters to your needs, ensuring a delightful journey from signup to checkout. Shop with confidence and manage with ease on our e-commerce platform!
